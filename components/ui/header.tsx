f1af82962285af424ea2394685510e48afc1e3ee
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-02-29 13:02:02 +0600

    ok

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -19,11 +19,7 @@ export default function Header() {
           <nav className="hidden md:flex md:grow">
             {/* Desktop sign in links */}
             <ul className="flex grow justify-end flex-wrap items-center">
-              <li>
-                <p className="font-medium text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">
-                  Home
-                </p>
-              </li>
+              
               <li>
                 <p className="font-medium text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">
                   Mission & Vission
@@ -31,7 +27,7 @@ export default function Header() {
               </li>
               <li>
                 <p className="font-medium text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">
-                  About Us
+                  IT Solution
                 </p>
               </li>
               <li>
